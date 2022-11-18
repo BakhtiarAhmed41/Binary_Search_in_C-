d24236da1849b74f18d23f36143d59225e0486aa
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2022-11-18 20:26:23 +0500

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -5,12 +5,12 @@ int A[]= {2,5,5,5,6,6,8,9,9,9};
 int n = sizeof(A)/sizeof(int);
 
 int binary_search(int A[], int item){
-    int x=0;
     int loc;
     int start = 0;
     int end = n-1;
     int mid = (start + end)/2;
-    while (loc < n){
+
+
     while(start<=end  && A[mid]!=item){
         if(item<A[mid]){
             end = mid -1;
@@ -19,21 +19,23 @@ int binary_search(int A[], int item){
             start = mid+1;
         }
         mid = (start + end)/2;
+
     }
-    if(A[mid]==item){
+
+        if(A[mid]==item){
         loc = mid;
-        x = x+1;
     }
-    else{
-        loc = -1;}
-    }
-    cout<<"The element "<<item<<" occurs "<<x<<" times.";
-    cout<<loc;
-    // return loc;
-    }
-    
+        else{
+        loc = -1;
+        }
+
+        return loc;
+  
+
+    cout<<"The element is present at index no."<<loc;
+}
 
 
 int main(){
-    binary_search(A, 6);
-}
+    binary_search(A, 9);
+    }
